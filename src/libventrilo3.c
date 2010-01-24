2d68a7aa8ecd70704a7d6b2b547a26fc605a8cc5
ekilfoil <ekilfoil@9ef036df-965f-46e4-a035-00cad25deb63>
2010-01-24 01:52:47 +0000

    adding a useless file to test the repo
    git-svn-id: http://svn.mangler.org/libventrilo3@2 9ef036df-965f-46e4-a035-00cad25deb63

---
+++ src/libventrilo3.c
@@ -0,0 +1,19 @@
+
+/*
+ * Initialize a libventrilo3 handle for a server connection.
+ * @return A pointer to a v3_handle for performing operations on a server
+ */
+v3_handle *v3_init(void);
+
+
+/*
+ * Free all resources allocated for the specified libventrilo3 handle and
+ * disconnect if currently connected
+ * @param v3h A v3_handle created by v3_init();
+ */
+void v3_free(v3_handle *v3h);
+
+/*
+ *
+ */
+
